23368b627210876add4a31d069aa448eb2840182
mac-00014 <mac-00014@example.com>
2019-11-28 11:50:03 +0530

    Initial Commit

---
+++ SingInWithApple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingInWithApple
+//
+//  Created by mac-00014 on 28/11/19.
+//  Copyright © 2019 Mind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
